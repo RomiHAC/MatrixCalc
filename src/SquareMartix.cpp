2afb327ea971e861ae155a4eee94b406fedf0976
The4ing <atar.orram101@gmail.com>
2025-03-20 16:12:17 +0200

    Update SquareMartix.cpp

--- src/SquareMartix.cpp
+++ src/SquareMartix.cpp
@@ -1,18 +1,28 @@
 #include "SquareMatrix.h"
 
-SquareMatrix::SquareMatrix(int n) 
-	: size(n), matrix(n, std::vector<int>(n,0)) {
-
-    std::cout << "Enter " << n * n << " values for the matrix:\n";
-
+SquareMatrix::SquareMatrix(int n)
+    : size(n), matrix(n, std::vector<int>(n, 0)) {
+    std::cout << "\nEnter " << n << "x" << n << " values for the matrix:\n";
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
-            std::cin >> matrix[i][j]; 
-            std::cout << matrix[i][i] <<" ";
+            std::cin >> matrix[i][j];
         }
-        std::cout << " /n";
     }
 }
 
+SquareMatrix::SquareMatrix(const std::vector<std::vector<int>>& mat)
+    : size(mat.size()), matrix(mat) {
+}
 
+std::vector<std::vector<int>> SquareMatrix::getMatrix() const {
+    return matrix;
+}
 
+void SquareMatrix::printMatrix() const {
+    for (const auto& row : matrix) {
+        for (int val : row) {
+            std::cout << val << " ";
+        }
+        std::cout << std::endl;
+    }
+}
