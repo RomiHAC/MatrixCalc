c4a0ffd52532a56a56dcf26ef0f8af99384c5720
The4ing <atar.orram101@gmail.com>
2025-03-20 16:11:19 +0200

    Update SquareMatrix.h

--- include/SquareMatrix.h
+++ include/SquareMatrix.h
@@ -2,17 +2,18 @@
 #include <iostream>
 #include <vector>
 
-
-
 class SquareMatrix {
-
 public:
-	SquareMatrix(int n);
-	
+
+    SquareMatrix(int n);
+    SquareMatrix(const std::vector<std::vector<int>>& mat);
+
+    std::vector<std::vector<int>> getMatrix() const;
+
+    void printMatrix() const;
 
 private:
-	int size;
-	std::vector<std::vector<int>> matrix;
-	
-
+    int size;
+    std::vector<std::vector<int>> matrix;
 };
+
