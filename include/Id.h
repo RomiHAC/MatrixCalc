534537cdbeefa6e6115fa362b1300b7b01540b2e
The4ing <atar.orram101@gmail.com>
2025-03-22 17:25:30 +0200

    Delete include directory

--- include/Id.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "Operation.h"
-
-class Id : public Operation {
-public:
-    SquareMatrix apply(const std::vector<SquareMatrix>& mat) const override;  
-};
--- include/Transpose.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "Operation.h"
-
-class Transpose : public Operation {
-public:
-    SquareMatrix apply(const std::vector<SquareMatrix>& mat) const override;
-};
