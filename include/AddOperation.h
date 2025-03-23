c2074050044349d908ee33ecf84e16f62d2d13c4
The4ing <atar.orram101@gmail.com>
2025-03-23 16:40:22 +0200

    Update AddOperation.h

--- include/AddOperation.h
+++ include/AddOperation.h
@@ -6,8 +6,8 @@ class AddOperation : public Operation {
 public:
 
     AddOperation(std::shared_ptr<Operation> op1, std::shared_ptr<Operation> op2);
-
     SquareMatrix apply(const std::vector<SquareMatrix>& matrices) const override;
+    int getNumMatrices() const override; 
 
 private:
     std::shared_ptr<Operation> op1;
