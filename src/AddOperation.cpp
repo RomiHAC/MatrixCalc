f72085eb0b437e91d9a25f49dcb86e5f63deb4e7
The4ing <atar.orram101@gmail.com>
2025-03-23 16:44:51 +0200

    Update AddOperation.cpp

--- src/AddOperation.cpp
+++ src/AddOperation.cpp
@@ -1,21 +1,23 @@
 #include "AddOperation.h"
 
 AddOperation::AddOperation(std::shared_ptr<Operation> op1, std::shared_ptr<Operation> op2)
- : op1(op1), op2(op2){}
+    : op1(op1), op2(op2) {
+}
 
 SquareMatrix AddOperation::apply(const std::vector<SquareMatrix>& matrices) const {
-    SquareMatrix mat_op1 = op1->apply(matrices);
-    SquareMatrix mat_op2 = op2->apply(matrices);
-
-    int size = mat_op1.getMatrix().size();
-    SquareMatrix result(size);
-
-    for (int i = 0; i < size; ++i) {
-        for (int j = 0; j < size; ++j) {
-            result.getMatrix()[i][j] = mat_op1.getMatrix()[i][j] + mat_op2.getMatrix()[i][j];
-        }
+    if (matrices.size() < 2) {
+        throw std::invalid_argument("Error: AddOperation requires at least two matrices.");
     }
-    return result;
+
+    SquareMatrix mat_op1 = op1->apply(matrices);
+    std::vector<SquareMatrix> matricesCopy = matrices;
+    matricesCopy[0] = matrices[1];
+    SquareMatrix mat_op2 = op2->apply(matricesCopy);
+
+    return mat_op1 + mat_op2;
 }
 
 
+int AddOperation::getNumMatrices() const {
+    return 2;
+}
