9b80d62cc8c68745063867cd0193fbb8e1dbbf08
The4ing <atar.orram101@gmail.com>
2025-03-23 16:43:57 +0200

    Update Scal.h

--- include/Scal.h
+++ include/Scal.h
@@ -3,12 +3,9 @@
 
 class Scal : public Operation {
 public:
-    // Constructor to initialize the scalar value
-    Scal(const int scalar);
-
-    // Override the apply method to perform scalar multiplication
+    Scal(int scalar);
     SquareMatrix apply(const std::vector<SquareMatrix>& matrices) const override;
+    int getNumMatrices() const override;
 private:
-    int scalar;  // Scalar value for multiplication
-
+    int scalar;
 };
