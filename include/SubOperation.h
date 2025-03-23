d759ab7b75910c220e3f53b60d89a81ace0f40de
The4ing <atar.orram101@gmail.com>
2025-03-23 16:44:22 +0200

    Update SubOperation.h

--- include/SubOperation.h
+++ include/SubOperation.h
@@ -6,8 +6,8 @@ class SubOperation : public Operation {
 
 public:
 	SubOperation(std::shared_ptr<Operation> op1, std::shared_ptr<Operation> op2);
-
 	SquareMatrix apply(const std::vector<SquareMatrix>& matrices) const override;
+	int getNumMatrices() const override;
 private:
 	std::shared_ptr<Operation> op1;
 	std::shared_ptr<Operation> op2;
