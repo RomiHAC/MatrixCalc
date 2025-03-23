13766007b8ba4e1be13965f34771862f672bd0ed
The4ing <atar.orram101@gmail.com>
2025-03-23 16:44:11 +0200

    Update SquareMatrix.h

--- include/SquareMatrix.h
+++ include/SquareMatrix.h
@@ -7,15 +7,23 @@ public:
 
     SquareMatrix(int n);
     SquareMatrix(const std::vector<std::vector<int>>& mat);
-
-   
     const std::vector<std::vector<int>>& getMatrix() const; // for constant access
-    std::vector<std::vector<int>>& getMatrix(); // for non-constant access
+    void setMatrix(const std::vector<std::vector<int>>& newMatrix);
+    void createMatrix();
 
+ //   std::vector<std::vector<int>> getMatrix() const;
     void printMatrix() const;
+
+    SquareMatrix operator+(const SquareMatrix& other) const;
+    SquareMatrix operator-(const SquareMatrix& other) const;
+    SquareMatrix operator*(const SquareMatrix& other) const;
+    SquareMatrix operator*(int scalar) const;
+    bool operator==(const SquareMatrix& other) const;
+
+    friend std::ostream& operator<<(std::ostream& out, const SquareMatrix& matrix);
+    friend std::istream& operator>>(std::istream& in, SquareMatrix& matrix);
 
 private:
     int size;
     std::vector<std::vector<int>> matrix;
 };
-
