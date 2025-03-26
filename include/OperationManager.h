bd6ba043ac805b5fd376a90920d730a51c1733fe
The4ing <atar.orram101@gmail.com>
2025-03-26 19:28:28 +0200

    Update OperationManager.h

--- include/OperationManager.h
+++ include/OperationManager.h
@@ -10,37 +10,24 @@
 #include "CompOperation.h"
 
 class OperationManager {
+public:
+
+    OperationManager();
+
+    int getOperationsCount() const;
+     const Help& getHelp()  ;
+    int addOperation(std::shared_ptr<Operation> op, const std::string& name);
+    void DeleteFunction(const int num);
+    SquareMatrix execute(int index, const std::vector<SquareMatrix>& matrices);
+    void listOperations() const;
+    bool OperationExists(const std::string& name) const;
+    bool CheckOptAndAdd(const std::string& command);
+    int getRequiredMatrixCount(int index);
+    
 private:
     std::vector<std::shared_ptr<Operation>> operations;  // Stores operations
     std::unordered_map<int, std::string> operationNames; // Maps index to operation name
     std::vector<int> requiredMatrices;
     Help help;
 
-public:
-
-    OperationManager();
-
-
-     const Help& getHelp()  ;
-
-    // Adds a new operation with a name and returns its index
-    int addOperation(std::shared_ptr<Operation> op, const std::string& name);
-
-    void DeleteFunction(const int num);
-
-    // Executes an operation by index, passing the required matrices
-    SquareMatrix execute(int index, const std::vector<SquareMatrix>& matrices);
-
-    // Prints the list of available operations
-    void listOperations() const;
-
-    bool OperationExists(const std::string& name) const;
-
-    bool CheckOptAndAdd(const std::string& command);
-
-    int getRequiredMatrixCount(int index);
-
-    
-
-    
 };
