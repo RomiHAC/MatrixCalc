88dcfa70c8b532a30742565e49e6a8849672e14d
The4ing <atar.orram101@gmail.com>
2025-03-26 19:29:26 +0200

    Update Help.cpp

--- src/Help.cpp
+++ src/Help.cpp
@@ -1,14 +1,14 @@
 #include "Help.h"
 
 void Help::printHelp() const {
-    std::cout << "\n1. eval(num, n) - Calculates the function for 'num' index using an 'nXn' matrix from the user." << std::endl;
-    std::cout << "2. scal(val) - Multiplies the matrix from 'eval' by the scalar 'val'." << std::endl;
-    std::cout << "3. add(num1, num2) - Adds the results of functions 'num1' and 'num2' (element-wise)." << std::endl;
-    std::cout << "4. sub(num1, num2) - Subtracts function 'num2' from 'num1' (element-wise)." << std::endl;
-    std::cout << "5. comp(num1, num2) - Applies function 'num1' first, then 'num2' on the result." << std::endl;
-    std::cout << "6. del(num) - Deletes function 'num' and updates the list." << std::endl;
-    std::cout << "7. help - Displays a list of commands with brief descriptions." << std::endl;
-    std::cout << "8. exit - Prints 'Goodbye' and exits the program." << std::endl;
+    std::cout << "\nThe available commands are :\n";
+    std::cout << "*eval(uate) num n - compute the result of operation #num on all\nneeded nxn matrices(that will be prompted)\n";
+    std::cout << "* scal(ar) val - creates an operation that multiplies the given\nmatrix by scalar val\n";
+    std::cout << "* add num1 num2 - creates an operation that is the addition of the\nresult of operation #num1 and the result of operation #num2\n";
+    std::cout << "* sub num1 num2 - creates an operation that is the subtraction of the\n result of operation #num1 and the result of operation #num2\n";
+    std::cout << "* comp(osite) num1 num2 - creates an operation that is the\ncomposition of operation #num1 and operation #num2\n";
+    std::cout << "* del(ete) num - delete operation #num from the operation list\n";
+    std::cout << "* help - print this command list\n";
+    std::cout << "* exit - exit the program\n";
     std::cout << std::endl;
-
 }
